a4bfb5fff1009fae10d498c8f828c95275e1a2ec
kevincode111 <kevin.abram@gojek.com>
2023-05-20 09:13:13 +0700

    modified package.swift for home and login

--- Modules/HomeModule/Package.swift
+++ Modules/HomeModule/Package.swift
@@ -6,21 +6,22 @@ import PackageDescription
 let package = Package(
     name: "HomeModule",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "HomeModule",
             targets: ["HomeModule"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        // MARK: Local Dependencies
+        .package(path: "../CommonModule"),
+        // MARK: Shared Dependencies
+        .package(path: "../../Shared/UtilityModule")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HomeModule",
-            dependencies: []),
+            dependencies: [
+                "CommonModule", "UtilityModule"
+            ]),
         .testTarget(
             name: "HomeModuleTests",
             dependencies: ["HomeModule"]),
