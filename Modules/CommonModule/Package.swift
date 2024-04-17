b398a1d1d40b3a2ed08d1815706b6b62eb64e879
kevincode111 <kevin.abram@gojek.com>
2024-04-17 17:08:16 +0700

    fixed utility module

--- Modules/CommonModule/Package.swift
+++ Modules/CommonModule/Package.swift
@@ -12,15 +12,16 @@ let package = Package(
             targets: ["CommonModule"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Swinject/Swinject", .exact("2.8.3")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CommonModule",
-            dependencies: []),
+            dependencies: [
+                "Swinject"
+            ]),
         .testTarget(
             name: "CommonModuleTests",
             dependencies: ["CommonModule"]),
